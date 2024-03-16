ed1b4cccf331c3d7f4927cce1faec368997104a5
mangat-ram <se.mangat.ram@gmail.com>
2024-03-16 10:24:25 +0530

    Added updations in Intro.

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,10 +3,13 @@
 import React from "react"
 import Image from "next/image"
 import { motion } from "framer-motion"
+import Link from "next/link";
+import { BsArrowRight } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -42,7 +45,34 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-      <p>I am a Software Engineer Dev</p>
+      <motion.p 
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity:0, y: 100 }}
+        animate={{ opacity:1, y:0 }}
+      >
+          <span className="font-bold">Hello I&apos;m Mangat.</span>I&apos;m a{" "}
+          <span className="font-bold">Full Stack Engineer and Dev</span> with{" "}
+          <span className="font-bold">1 year</span> of experience.I enjoy building
+          <span className="italic"> Sites and Apps</span>. My Focus is{" "}
+          <span className="underline">NodeJs,ExpressJs and React.</span>
+      </motion.p>
+      <div>
+        <Link 
+          href="#contact"
+          className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full"
+        >
+          Contact Me Here
+          <BsArrowRight />
+        </Link>
+        <a 
+          href=""
+          className="bg-white px-7 py-3 flex items-center gap-2 rounded-full"
+        >
+          Download CV
+          <HiDownload />
+        </a>
+      </div>
+
     </section>
   )
 }
