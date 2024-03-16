8ec642a7e9b8c2cd677804a74e0dfbad8f832b36
mangat-ram <se.mangat.ram@gmail.com>
2024-03-16 12:18:31 +0530

    Completed Section Divider Component.

--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function SectionDivider() {
   return (
-    <div>SectionDivider</div>
+    <div
+      className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block"
+    >
+    </div>
   )
 }
