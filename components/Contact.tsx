7cd0b1c76053672986edff7f357144a006829d37
mangat-ram <se.mangat.ram@gmail.com>
2024-03-17 18:39:44 +0530

    completed contact me component.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,22 +1,52 @@
+"use client";
+
 import React from 'react'
 import SectionHeading from './SectionHeading'
 import { FaPaperPlane } from 'react-icons/fa6'
+import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function Contact() {
+
+  const { ref } = useSectionInView("Contact")
+
   return (
-    <section
+    <motion.section
+      ref={ref}
       id="contact"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)]"
+      initial={{opacity:0}}
+      whileInView={{opacity:1}}
+      transition={{duration:1}}
+      viewport={{once:true}}
     >
       <SectionHeading>Contact</SectionHeading>
-      <p>Please Contact me directly at <a className="underline" href="mailto:se.mangat.ram@gmail.com">se.mangat.ram@gmail.com</a> or through this form.</p>
-      <form action="">
-        <input type="email" />
-        <textarea />
-        <button type="submit">
+      <p 
+        className="text-gray-700 -mt-6"
+      >Please Contact me directly at <a className="underline" href="mailto:se.mangat.ram@gmail.com">se.mangat.ram@gmail.com</a> or through this form.</p>
+      <form className="mt-10 flex flex-col">
+        <input 
+        className="h-14 px-4 rounded-lg borderBlack"
+        type="email" 
+        placeholder="Your email"
+        />
+        <textarea 
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+          placeholder="Your message"
+        />
+        <button 
+          type="submit"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all
+          focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950"
+        >
           Submit
-          <FaPaperPlane />
+          <FaPaperPlane
+            className="text-xs opacity-70 transition-all
+            group-hover:translate-x-1
+            group-hover:-translate-y-1"
+          />{" "}
         </button>
       </form>
-    </section>
+    </motion.section>
   )
 }
