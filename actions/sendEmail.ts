501b16704fbfea340a2feddb37e824824c2fa543
mangat-ram <se.mangat.ram@gmail.com>
2024-03-18 21:06:49 +0530

    Writing email logic.

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,18 @@
 "use server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendEmail = async (formData : FormData) => {
     console.log("Running on server.");
     console.log(formData.get("senderEmail"));
     console.log(formData.get("message"));
+
+    resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to:'mram34272@gmail.com',
+      subject:'Message from your Portfolio contact form',
+      // text:`Message from ${formData.get("senderEmail")}: ${formData.get("message")}`
+      text:`Hello World!!`
+    })
   }
